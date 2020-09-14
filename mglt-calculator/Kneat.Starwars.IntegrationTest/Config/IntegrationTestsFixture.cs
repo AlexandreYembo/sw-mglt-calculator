57d315ae60a8efd98446633dffd25dd7afab5ad8
Alexandre Yembo <alexandreyembo@Alexandres-MacBook-Pro.local>
2020-09-14 13:07:33 +0100

    Update IntegrationTestsFixture.cs

--- mglt-calculator/Kneat.Starwars.IntegrationTest/Config/IntegrationTestsFixture.cs
+++ mglt-calculator/Kneat.Starwars.IntegrationTest/Config/IntegrationTestsFixture.cs
@@ -5,9 +5,17 @@ using Xunit;
 
 namespace Kneat.Starwars.IntegrationTest.Config
 {
+    /// <summary>
+    /// Collection will apply for all test class that are member of this collection. You have to declare this collection
+    /// as attribute of the Test Class
+    /// </summary>
     [CollectionDefinition(nameof(IntegrationTestsFixtureCollection))]
     public class IntegrationTestsFixtureCollection: ICollectionFixture<IntegrationTestsFixture>{ }
-    
+
+    /// <summary>
+    /// This fixture created once the instance of objects and it does not call for each test that starts, 
+    /// because it saves the state of the object.
+    /// </summary>    
     public class IntegrationTestsFixture
     {
         
